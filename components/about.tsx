734ce7dd66a3cebadc0a6ada439f47b99bdae6de
Shyam Kumar <contactskchauhan@gmail.com>
2023-09-16 12:41:32 +0530

    updating about

--- components/about.tsx
+++ components/about.tsx
@@ -19,8 +19,8 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
+        After completing with a Polytechnic in{" "}
+        <span className="font-medium">Computer Science</span>, I decided to pursue my
         passion for programming. I enrolled in a coding bootcamp and learned{" "}
         <span className="font-medium">full-stack web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
@@ -28,7 +28,7 @@ export default function About() {
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+          React, Next.js, Node.js, .NET, SQL and MongoDB
         </span>
         . I am also familiar with TypeScript and Prisma. I am always looking to
         learn new technologies. I am currently looking for a{" "}
@@ -41,8 +41,8 @@ export default function About() {
         video games, watching movies, and playing with my dog. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="font-medium">Ancient Indian history and philosophy</span>. I'm also
+        learning Sanskrit so that I can De-Code the Vedas.
       </p>
     </motion.section>
   );
